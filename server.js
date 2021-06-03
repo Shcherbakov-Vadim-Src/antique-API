3060fea9a348334b1ed95be2baaf1f147ed1f923
vadim <79169082210@mail.ru>
2021-06-03 12:19:43 +0300

    added photo upload

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-
 const { v4: uuidv4 } = require('uuid');
+const cors = require('cors');
+const multer = require('multer')
+// const upload = multer({ dest: 'uploads/' })
+
+let fileNamePhoto = '';
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads')
+    },
+    filename: function (req, file, cb) {
+
+        // console.log(file);
+        fileNamePhoto = file.originalname;
+        cb(null, file.originalname)
+    }
+})
+
+let upload = multer({ storage: storage })
 
 const Good = require('./Good');
 
@@ -10,8 +28,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.static(__dirname + '/public'));
 
-mongoose.connect('mongodb+srv://Vados:12345@cluster0.jt9sp.mongodb.net/antique?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connect('mongodb+srv://Vados:12345@cluster0.jt9sp.mongodb.net/antique?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.on('error', (err) => {
@@ -24,9 +45,7 @@ db.once('open', () => {
 
 // category: jewelry, accessories, icons, ancient values, household utensils
 
-// app.get('/api/goods', (req, res) => {                // годно
-//     res.send(goods);
-// });
+
 
 app.get('/api/goods', (req, res) => {                 // годно
     Good.find().then((good) => {
@@ -39,8 +58,8 @@ app.get('/api/goods', (req, res) => {                 // годно
 app.get('/api/goods/:id', (req, res) => {         // годно
     // const id = req.params.id;
     // console.log('------>', req.params.id);
-    Good.find({_id: req.params.id}).then((data) => {
-        if (data){
+    Good.find({ _id: req.params.id }).then((data) => {
+        if (data) {
             res.status(200).json(data);
         } else {
             res.status(404).send();
@@ -48,41 +67,45 @@ app.get('/api/goods/:id', (req, res) => {         // годно
     })
 })
 
-// app.post('/api/goods', (req, res) => {                  // годно
-//     // console.log(req.body);
-//     const good = new Good(req.body);
-//     good.save((err) => {
-//         if (err) {
+
+// app.put('/api/goods/:id', (req, res) => {                 // годно
+//     console.log(req.body);
+//     Good.updateOne({ _id: req.params.id }, req.body)
+//         .then(() => {
+//             res.status(204).send();
+//         })
+//         .catch((err) => {
 //             console.log(err);
-//             return res.status(500).send();
-//         }
-//         console.log('Success');
-//         res.status(201).send();
-//     }); 
-// });
-
-app.put('/api/goods/:id', (req, res) => {                 // годно
-    Good.updateOne({ _id: req.params.id }, req.body)
-    .then(() => {
-        res.status(204).send();
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-})
-
-// app.delete('/api/goods/:id', (req, res) => {              // годно
-    // Good.deleteOne({ _id: req.params.id })
-    // .then(() => {
-    //     res.status(204).send();
-    // })
-    // .catch((err) => {
-    //     console.log(err);
-    // })
+//         })
 // })
 
+app.put('/api/goods/:id', upload.single('avatar'), (req, res) => {                 // годно
+    
+    console.log('------->req.body', req.body);
 
-const tokens = [ 
+    const newGood = {
+        category: req.body.category,
+        title: req.body.title,
+        price: req.body.price,
+        about: req.body.about,
+        photo: `http://localhost:3025/uploads/${fileNamePhoto}`,
+        dateOfPlacement: req.body.date,
+        dateOfSale: ''
+    };
+
+    console.log('------->newGood', newGood);
+
+    Good.updateOne({ _id: req.params.id }, newGood)
+        .then(() => {
+            res.status(204).send();
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
+
+
+const tokens = [
     {
         // login: "admin",
         // password: "12345"
@@ -90,20 +113,45 @@ const tokens = [
 ];
 
 app.post('/token', (req, res) => {                                    // создание токена
-  const { login, password } = req.body;
-  if (login === 'admin' && password === '12345'){
-    const newToken = uuidv4();
-    // console.log(newToken);
-    tokens.push({ login: login, token: newToken });
-    res.json({ login: login, token: newToken });
-  } else {
-    res.status(400).json({ err: 'Can not find user' })
-  }
+    const { login, password } = req.body;
+    if (login === 'admin' && password === '12345') {
+        const newToken = uuidv4();
+        // console.log(newToken);
+        tokens.push({ login: login, token: newToken });
+        res.json({ login: login, token: newToken });
+    } else {
+        res.status(400).json({ err: 'Can not find user' })
+    }
+})
+
+app.post('/addgoods', upload.single('avatar'), (req, res) => {
+
+    const newGood = {
+        category: req.body.category,
+        title: req.body.title,
+        price: req.body.price,
+        about: req.body.about,
+        photo: `http://localhost:3025/uploads/${fileNamePhoto}`,
+        dateOfPlacement: req.body.date,
+        dateOfSale: ''
+    };
+
+    const good = new Good(newGood);
+    good.save((err) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send();
+        }
+        console.log('Success');
+        res.status(201).send();
+    });
+
 })
 
 
 app.post('/api/goods', (req, res) => {                                   // годно c использованием токена
     const { token } = req.body;
+
     // console.log(token);
     const isLoginValid = tokens.find((t) => t.token === token);
     console.log(token);
@@ -116,9 +164,9 @@ app.post('/api/goods', (req, res) => {                                   // го
             }
             console.log('Success');
             res.status(201).send();
-            });
-        } else {
-            res.status(401).json({ error: 'The user does not exist' });
+        });
+    } else {
+        res.status(401).json({ error: 'The user does not exist' });
     }
 });
 
@@ -129,26 +177,16 @@ app.delete('/api/goods/:id', (req, res) => {                                    
     if (isLoginValid) {
 
         Good.deleteOne({ _id: req.params.id })
-        .then(() => {
-            res.status(204).send();
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-
+            .then(() => {
+                res.status(204).send();
+            })
+            .catch((err) => {
+                console.log(err);
+            })
     }
-    // const { _id, token } = req.body;
-    // const isTokenValid = tokens.find((t) => t.token === token);
-    // if (isTokenValid) {
-    //   Good = Good.filter(good => good._id === _id);                              // Good =
-    //   res.send({ success: true });
-    // } else {
-    //   res.status(401).send({error: 'Not authorized' });
-    // }
-  });
+});
 
 
 app.listen(3025, () => {
     console.log('server on 3025 port is connected');
 })
-
